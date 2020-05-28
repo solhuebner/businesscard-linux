e336ca96b40211f1e4523a41359bffa8393a1bcb
xuefengedu <1162036989@qq.com>
2020-05-28 02:59:59 -0700

    Add ninvaders game

---
+++ package/ninvaders/ninvaders.mk
@@ -0,0 +1,26 @@
+################################################################################
+#
+# nInvaders
+#
+################################################################################
+
+NINVADERS_VERSION = 0.1.1
+NINVADERS_SOURCE = ninvaders-$(NINVADERS_VERSION).tar.gz
+NINVADERS_SITE = https://downloads.sourceforge.net/project/ninvaders/ninvaders/$(NINVADERS_VERSION)
+NINVADERS_DEPENDENCIES = ncurses libseccomp
+
+define NINVADERS_BUILD_CMDS
+    $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) all
+endef
+
+define NINVADERS_INSTALL_TARGET_CMDS
+    $(INSTALL) -D -m 0755 $(@D)/nInvaders $(TARGET_DIR)/usr/bin
+endef
+
+#Add user ninvaders
+define NINVADERS_USERS
+    ninvaders -1 ninvaders -1 !=ninvaders /home - -
+endef
+
+$(eval $(generic-package))
+
