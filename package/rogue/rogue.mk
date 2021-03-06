8fc7e23a65d0ccebbbb894a10b89c313c45e838c
Sol Huebner <solhuebner@users.noreply.github.com>
2021-03-06 12:54:36 -0500

    replace rogue with own version

--- package/rogue/rogue.mk
+++ package/rogue/rogue.mk
@@ -4,15 +4,15 @@
 #
 ################################################################################
 
-ROGUE_VERSION = 5.4.4
-ROGUE_SITE = https://github.com/phs/rogue/archive
+ROGUE_VERSION = 5.3-4.4.2020.1
+ROGUE_SITE = https://github.com/solhuebner/rogue/archive
 ROGUE_SOURCE = v$(ROGUE_VERSION).tar.gz
 ROGUE_LICENSE = Custom
 ROGUE_LICENSE_FILES = LICENSE.TXT
 ROGUE_DEPENDENCIES = ncurses
 
 ROGUE_CONF_OPTS = \
-	--enable-scorefile=/root/.rogue.scr \
-	--enable-lockfile=/tmp/rogue.lck \
+        --enable-ncurses \
+        --enable-wizardmode \
 
 $(eval $(autotools-package))
